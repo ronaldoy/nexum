5527969fb1a6d29bb470e687b1c675c722ca2f6e
Ronaldo Pagani Yamashita <ronaldoy@gmail.com>
2026-02-20 13:44:27 -0300

    Refactor session cookie authentication concern

--- app/app/controllers/concerns/authentication.rb
+++ app/app/controllers/concerns/authentication.rb
@@ -16,143 +16,148 @@ module Authentication
   end
 
   private
-    def authenticated?
-      resume_session
+  def authenticated?
+    resume_session
+  end
+
+  def require_authentication
+    resume_session || request_authentication
+  end
+
+  def resume_session
+    Current.session ||= find_session_by_cookie
+    Current.user ||= Current.session&.user
+  end
+
+  def find_session_by_cookie
+    cookie_identity = session_cookie_identity
+    return unless valid_session_cookie_identity?(cookie_identity)
+
+    bootstrap_database_tenant_context!(cookie_identity[:tenant_id])
+    session = find_persisted_session(cookie_identity)
+    return clear_bootstrap_database_tenant_context! && nil unless session
+
+    return reject_persisted_session(session) unless valid_session_user_tenant?(session, cookie_identity[:tenant_id])
+    return reject_persisted_session(session) if invalid_session_user_uuid?(session, cookie_identity[:user_uuid_id])
+    return reject_persisted_session(session) unless valid_session_fingerprint?(session)
+    return reject_persisted_session(session) if session.expired?
+
+    session
+  end
+
+  def session_cookie_identity
+    {
+      session_id: cookies.encrypted[:session_id],
+      tenant_id: cookies.encrypted[SESSION_TENANT_COOKIE_KEY],
+      user_uuid_id: cookies.encrypted[SESSION_USER_UUID_COOKIE_KEY]
+    }
+  end
+
+  def valid_session_cookie_identity?(cookie_identity)
+    cookie_identity[:session_id].present? && cookie_identity[:tenant_id].present?
+  end
+
+  def find_persisted_session(cookie_identity)
+    Session.includes(:user).find_by(id: cookie_identity[:session_id], tenant_id: cookie_identity[:tenant_id])
+  end
+
+  def valid_session_user_tenant?(session, tenant_id)
+    session.user&.tenant_id.to_s == tenant_id.to_s
+  end
+
+  def invalid_session_user_uuid?(session, user_uuid_id)
+    user_uuid_id.present? && session.user.uuid_id.to_s != user_uuid_id.to_s
+  end
+
+  def reject_persisted_session(session)
+    terminate_persisted_session(session)
+    clear_bootstrap_database_tenant_context!
+    nil
+  end
+
+  def request_authentication
+    session[:return_to_after_authenticating] = request.url
+    redirect_to new_session_path
+  end
+
+  def after_authentication_url
+    session.delete(:return_to_after_authenticating) || root_url
+  end
+
+  def start_new_session_for(user)
+    reset_session
+
+    user.sessions.create!(tenant: user.tenant, user_agent: request.user_agent, ip_address: request.remote_ip).tap do |session|
+      Current.session = session
+      Current.user = user
+      assign_session_cookies(session:, user:)
     end
+  end
 
-    def require_authentication
-      resume_session || request_authentication
-    end
+  def assign_session_cookies(session:, user:)
+    write_encrypted_session_cookie(:session_id, session.id)
+    write_encrypted_session_cookie(SESSION_TENANT_COOKIE_KEY, session.tenant_id)
+    write_encrypted_session_cookie(SESSION_USER_UUID_COOKIE_KEY, user.uuid_id)
+  end
 
-    def resume_session
-      Current.session ||= find_session_by_cookie
-      Current.user ||= Current.session&.user
-    end
+  def write_encrypted_session_cookie(key, value)
+    cookies.encrypted[key] = { value: value, **session_cookie_options }
+  end
 
-    def find_session_by_cookie
-      session_id = cookies.encrypted[:session_id]
-      tenant_id = cookies.encrypted[SESSION_TENANT_COOKIE_KEY]
-      user_uuid_id = cookies.encrypted[SESSION_USER_UUID_COOKIE_KEY]
-      return unless session_id && tenant_id
+  def terminate_session
+    terminate_persisted_session(Current.session)
+    Current.session = nil
+    Current.user = nil
+    clear_session_cookies
+    clear_bootstrap_database_tenant_context!
+  end
 
-      bootstrap_database_tenant_context!(tenant_id)
+  def terminate_persisted_session(session)
+    session&.destroy
+    clear_session_cookies
+  end
 
-      session = Session.includes(:user).find_by(id: session_id, tenant_id: tenant_id)
-      unless session
-        clear_bootstrap_database_tenant_context!
-        return nil
-      end
+  def clear_session_cookies
+    cookies.delete(:session_id)
+    cookies.delete(SESSION_TENANT_COOKIE_KEY)
+    cookies.delete(SESSION_USER_UUID_COOKIE_KEY)
+  end
 
-      user = session.user
-      unless user&.tenant_id.to_s == tenant_id.to_s
-        terminate_persisted_session(session)
-        clear_bootstrap_database_tenant_context!
-        return nil
-      end
+  def session_cookie_options
+    {
+      expires: Session.ttl.from_now,
+      httponly: true,
+      secure: secure_session_cookies?,
+      same_site: :strict
+    }
+  end
 
-      if user_uuid_id.present? && user.uuid_id.to_s != user_uuid_id.to_s
-        terminate_persisted_session(session)
-        clear_bootstrap_database_tenant_context!
-        return nil
-      end
+  def secure_session_cookies?
+    configured = Rails.app.creds.option(:security, :secure_session_cookies, default: ENV["SECURE_SESSION_COOKIES"])
+    return ActiveModel::Type::Boolean.new.cast(configured) unless configured.nil?
 
-      unless valid_session_fingerprint?(session)
-        terminate_persisted_session(session)
-        clear_bootstrap_database_tenant_context!
-        return nil
-      end
+    !Rails.env.development? && !Rails.env.test?
+  end
 
-      return session unless session.expired?
+  def valid_session_fingerprint?(session)
+    return false if session.blank?
+    return false if session.ip_address.present? && enforce_session_ip_binding? && session.ip_address != request.remote_ip
+    return false if session.user_agent.present? && enforce_session_user_agent_binding? && session.user_agent != request.user_agent.to_s
 
-      terminate_persisted_session(session)
-      clear_bootstrap_database_tenant_context!
-      nil
-    end
+    true
+  end
 
-    def request_authentication
-      session[:return_to_after_authenticating] = request.url
-      redirect_to new_session_path
-    end
+  def enforce_session_ip_binding?
+    configured = Rails.app.creds.option(:security, :session_bind_ip, default: ENV["SESSION_BIND_IP"])
+    return ActiveModel::Type::Boolean.new.cast(configured) unless configured.nil?
 
-    def after_authentication_url
-      session.delete(:return_to_after_authenticating) || root_url
-    end
+    true
+  end
 
-    def start_new_session_for(user)
-      reset_session
+  def enforce_session_user_agent_binding?
+    configured = Rails.app.creds.option(:security, :session_bind_user_agent, default: ENV["SESSION_BIND_USER_AGENT"])
+    return ActiveModel::Type::Boolean.new.cast(configured) unless configured.nil?
 
-      user.sessions.create!(tenant: user.tenant, user_agent: request.user_agent, ip_address: request.remote_ip).tap do |session|
-        Current.session = session
-        Current.user = user
-        cookies.encrypted[:session_id] = {
-          value: session.id,
-          **session_cookie_options
-        }
-        cookies.encrypted[SESSION_TENANT_COOKIE_KEY] = {
-          value: session.tenant_id,
-          **session_cookie_options
-        }
-        cookies.encrypted[SESSION_USER_UUID_COOKIE_KEY] = {
-          value: user.uuid_id,
-          **session_cookie_options
-        }
-      end
-    end
-
-    def terminate_session
-      terminate_persisted_session(Current.session)
-      Current.session = nil
-      Current.user = nil
-      clear_session_cookies
-      clear_bootstrap_database_tenant_context!
-    end
-
-    def terminate_persisted_session(session)
-      session&.destroy
-      clear_session_cookies
-    end
-
-    def clear_session_cookies
-      cookies.delete(:session_id)
-      cookies.delete(SESSION_TENANT_COOKIE_KEY)
-      cookies.delete(SESSION_USER_UUID_COOKIE_KEY)
-    end
-
-    def session_cookie_options
-      {
-        expires: Session.ttl.from_now,
-        httponly: true,
-        secure: secure_session_cookies?,
-        same_site: :strict
-      }
-    end
-
-    def secure_session_cookies?
-      configured = Rails.app.creds.option(:security, :secure_session_cookies, default: ENV["SECURE_SESSION_COOKIES"])
-      return ActiveModel::Type::Boolean.new.cast(configured) unless configured.nil?
-
-      !Rails.env.development? && !Rails.env.test?
-    end
-
-    def valid_session_fingerprint?(session)
-      return false if session.blank?
-      return false if session.ip_address.present? && enforce_session_ip_binding? && session.ip_address != request.remote_ip
-      return false if session.user_agent.present? && enforce_session_user_agent_binding? && session.user_agent != request.user_agent.to_s
-
-      true
-    end
-
-    def enforce_session_ip_binding?
-      configured = Rails.app.creds.option(:security, :session_bind_ip, default: ENV["SESSION_BIND_IP"])
-      return ActiveModel::Type::Boolean.new.cast(configured) unless configured.nil?
-
-      true
-    end
-
-    def enforce_session_user_agent_binding?
-      configured = Rails.app.creds.option(:security, :session_bind_user_agent, default: ENV["SESSION_BIND_USER_AGENT"])
-      return ActiveModel::Type::Boolean.new.cast(configured) unless configured.nil?
-
-      true
-    end
+    true
+  end
 end
